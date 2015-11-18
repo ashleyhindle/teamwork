e7061abec60b9221f66652f0b2ff28f8790490a8
Ashley Hindle <hey@ashleyhindle.com>
2015-11-18 22:39:15 +0000

    Added an Authenticate class to find a users URL based on their API key

--- src/Rossedman/Teamwork/Authenticate.php
+++ src/Rossedman/Teamwork/Authenticate.php
@@ -2,11 +2,11 @@
 
 class Authenticate extends AbstractObject {
 
-    protected $url = "http://authenticate.teamworkpm.net/authenticate.json";
+    protected $url = "https://authenticate.teamworkpm.net/authenticate.json";
 
     /**
      * Authenticate Details
-     * GET http://authenticate.teamworkpm.net/authenticate.json
+     * GET https://authenticate.teamworkpm.net/authenticate.json
      *
      * @link http://developer.teamwork.com/account#the_'authenti
      * @return mixed
